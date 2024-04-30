b8fd06664e39746f2266dddcdeb198562c403279
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-04-30 09:18:50 +0200

    Dockerfile

--- server.py
+++ server.py
@@ -1,14 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 
-app = Flask(__name__, static_folder='app/web')
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory(app.static_folder, path)
+def hello():
+    return "Hello from Flask!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
