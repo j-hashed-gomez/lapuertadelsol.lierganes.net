8cbe446c680265a84cfe4e94a18e21857a3e068f
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-06-01 10:28:20 +0200

    CE-11824

--- web/static/descargar.php
+++ web/static/descargar.php
@@ -2,28 +2,36 @@
 $upload_dir = "/var/www/html/uploads/";
 $zip_file = $upload_dir . "lapuertadelsol.zip";
 
-// Comprime los archivos .txt en un archivo zip
+// Comprime los archivos .txt y .png en un archivo zip
 $zip = new ZipArchive();
 if ($zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
-    $txt_files = glob($upload_dir . "*.txt");
-    foreach ($txt_files as $file) {
+    // Busca archivos .txt y .png
+    $all_files = glob($upload_dir . "*.{txt,png}", GLOB_BRACE);
+    
+    // Agrega todos los archivos encontrados al ZIP
+    foreach ($all_files as $file) {
         $zip->addFile($file, basename($file));
     }
+
+    // Cierra el archivo ZIP
     $zip->close();
+} else {
+    echo "Lo siento, no se pudo abrir el archivo zip para su creación.";
+    exit();
 }
 
 // Descarga el archivo zip
 if (file_exists($zip_file)) {
     header('Content-Description: File Transfer');
     header('Content-Type: application/zip');
-    header('Content-Disposition: attachment; filename="'.basename($zip_file).'"');
+    header('Content-Disposition: attachment; filename="' . basename($zip_file) . '"');
     header('Content-Length: ' . filesize($zip_file));
     readfile($zip_file);
-    
+
     // Borra el archivo zip después de ser descargado
     unlink($zip_file);
     exit();
 } else {
-    echo "Lo siento, no se pudo crear el archivo zip.";
+    echo "Lo siento, no se pudo encontrar el archivo zip para la descarga.";
 }
 ?>
